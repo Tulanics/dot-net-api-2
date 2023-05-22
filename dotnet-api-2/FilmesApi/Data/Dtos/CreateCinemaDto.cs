faffd648494e1b46970c4fe5b6189716211627e3
Tulani Conceição da Silva <76886738+Tulanics@users.noreply.github.com>
2023-05-22 19:18:45 -0300

    Projeto Inicial
    Projeto inicial com a adição da Classe Cinema, Dtos, Controller

---
+++ dotnet-api-2/FilmesApi/Data/Dtos/CreateCinemaDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Data.Dtos
+{
+    public class CreateCinemaDto
+    {
+        [Required(ErrorMessage = "O Campo de nome é Obrigatório. ")]
+        public string Nome { get; set; }
+    }
+}
---
+++ dotnet-api-2/FilmesApi/Models/Cinema.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Models
+{
+    public class Cinema
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "O Campo de nome é Obrigatório. ")]
+        public string Nome { get; set; }
+    }
+}
